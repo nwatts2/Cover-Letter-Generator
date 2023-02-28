d3f56a91877dad50c4faa175a178d81a4f9da450
Noah Watts <nwatts2@uccs.edu>
2023-02-28 13:56:56 -0700

    Initial Commit

---
+++ Cover Letter Generator/Cover_Letter_GeneratorApp.swift
@@ -0,0 +1,17 @@
+//
+//  Cover_Letter_GeneratorApp.swift
+//  Cover Letter Generator
+//
+//  Created by Nuah on 2/28/23.
+//
+
+import SwiftUI
+
+@main
+struct Cover_Letter_GeneratorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
